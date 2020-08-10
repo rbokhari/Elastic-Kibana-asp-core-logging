32d9ab8a515fd498ba2a8879a202421c6e45287d
Syed Rahman Bokhari <rbokhari@gmail.com>
2020-08-10 16:01:53 -0400

    Elastic search and kibana logging for asp.net core

---
+++ Elastic.Kibana.Serilog/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Elastic.Kibana.Serilog.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Elastic.Kibana.Serilog.Controllers
+{
+
+
+    public class HomeController : Controller
+    {
+        ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("HomeController Index executed at {date}", DateTime.UtcNow);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
